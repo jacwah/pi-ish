4959276b30a3aa4a6bb20869e5f8660138582ae6
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2017-04-08 19:25:42 +0200

    Implement estimation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate rand;
+
+use rand::Rng;
+
+fn circle_contains(radius: i32, x: i32, y: i32) -> bool {
+   radius.pow(2) >= x.pow(2) + y.pow(2)
+}
+    
+
 fn main() {
-    println!("Hello, world!");
+    let radius = 1_000;
+    let iterations = 1_000_000;
+
+    let mut num_in_circle = 0;
+
+    for _ in 0..iterations {
+        if circle_contains(radius, 
+                          rand::thread_rng().gen_range(-radius, radius),
+                          rand::thread_rng().gen_range(-radius, radius))
+        {
+            num_in_circle += 1;
+        }
+    }
+
+    let estimation = 4.0 * (num_in_circle as f64) / (iterations as f64);
+
+    println!("π ≈ {}", estimation);
 }
