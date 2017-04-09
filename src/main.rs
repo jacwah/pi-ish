817090c58ed02744b084723b47953bf05cc83c5b
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2017-04-09 21:32:34 +0200

    Calculate on multiple threads

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 extern crate rand;
+extern crate num_cpus;
 
 use rand::Rng;
+use std::thread;
 
 /// Compute if the point (x, y) is inside the radius of the origin.
-fn circle_contains(radius: i32, x: i32, y: i32) -> bool {
-   radius.pow(2) >= x.pow(2) + y.pow(2)
+fn circle_contains(radius: f64, x: f64, y: f64) -> bool {
+   radius.powi(2) >= x.powi(2) + y.powi(2)
 }
 
 /// Estimate π using a Monte Carlo simulation.
-fn estimate_pi(radius: i32, iterations: i32) -> f64 {
+fn estimate_pi(iterations: i32) -> f64 {
+    let radius = 1.0;
+    let mut rng = rand::thread_rng();
     let mut num_in_circle = 0;
-    let sample = || rand::thread_rng().gen_range(-radius, radius);
+
+    let mut sample = || rng.gen_range(-radius, radius);
 
     for _ in 0..iterations {
         if circle_contains(radius, sample(), sample()) {
@@ -22,10 +27,22 @@ fn estimate_pi(radius: i32, iterations: i32) -> f64 {
 }
 
 fn main() {
-    let radius = 1_000;
-    let iterations = 1_000_000;
+    let iterations = 1_000_000_000;
+    let num_workers = num_cpus::get();
 
-    println!("π ≈ {}", estimate_pi(radius, iterations));
+    let mut workers = vec![];
+
+    for _ in 0..num_workers {
+       workers.push(thread::spawn(move || {
+          estimate_pi(iterations)
+      }));
+    }
+
+    let estimation = workers.into_iter()
+        .map(|worker| worker.join().unwrap())
+        .sum::<f64>() / num_workers as f64;
+
+    println!("π ≈ {}", estimation);
 }
 
 
